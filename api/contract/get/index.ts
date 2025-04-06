e67cce055fd1349d85dfab808b0f68c0f2d5211e
nancy-kataria <katarianancy8@gmail.com>
2025-04-06 13:02:29 -0700

    get contract http request

---
+++ api/contract/get/index.ts
@@ -0,0 +1,31 @@
+import { Router } from "@fartlabs/rt";
+import { db } from "./../../../database/db.ts";
+
+export const getContractRouter = new Router()
+  .get("/api/contract/get/:id", async ({params}) => {
+    const contractId = params?.pathname.groups?.id;
+    
+    if (!contractId) {
+      return new Response(JSON.stringify({ error: "Invalid contract ID" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const result = await db.get(["contract", contractId]);
+    // if (!result.value) {
+    //   return new Response(JSON.stringify({ error: "Contract not found" }), {
+    //     status: 404,
+    //     headers: { "Content-Type": "application/json" },
+    //   });
+    // }
+
+    console.log(JSON.stringify(result.value))
+
+    return new Response(JSON.stringify(result.value), {
+      headers: { "Content-Type": "application/json" },
+    });
+  })
+  .default(() => new Response("Not found", { status: 404 }));
+
+Deno.serve((request) => getContractRouter.fetch(request));
