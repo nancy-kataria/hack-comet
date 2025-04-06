33262f3c8a5839f8e2a9916c4fce3f401d52f0fc
nancy-kataria <katarianancy8@gmail.com>
2025-04-06 13:23:44 -0700

    update get request

--- api/contract/get/index.ts
+++ api/contract/get/index.ts
@@ -2,9 +2,9 @@ import { Router } from "@fartlabs/rt";
 import { db } from "./../../../database/db.ts";
 
 export const getContractRouter = new Router()
-  .get("/api/contract/get/:id", async ({params}) => {
+  .get("/api/contract/get/:id", async ({ params }) => {
     const contractId = params?.pathname.groups?.id;
-    
+
     if (!contractId) {
       return new Response(JSON.stringify({ error: "Invalid contract ID" }), {
         status: 400,
@@ -13,19 +13,19 @@ export const getContractRouter = new Router()
     }
 
     const result = await db.get(["contract", contractId]);
-    // if (!result.value) {
-    //   return new Response(JSON.stringify({ error: "Contract not found" }), {
-    //     status: 404,
-    //     headers: { "Content-Type": "application/json" },
-    //   });
-    // }
 
-    console.log(JSON.stringify(result.value))
+    if (!result.value) {
+      return new Response(JSON.stringify({ error: "Contract not found" }), {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    console.log(JSON.stringify(result.value));
 
     return new Response(JSON.stringify(result.value), {
+      status: 200,
       headers: { "Content-Type": "application/json" },
     });
   })
   .default(() => new Response("Not found", { status: 404 }));
-
-Deno.serve((request) => getContractRouter.fetch(request));
