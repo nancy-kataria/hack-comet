0ca3df245b4ef55ea58f13432141e07691d0fe33
nancy-kataria <katarianancy8@gmail.com>
2025-04-06 21:27:08 -0700

    added validation

--- models/contract.ts
+++ models/contract.ts
@@ -1,10 +1,7 @@
-import { getPrototypeValue } from "@fartlabs/declarative";
-import { docOf } from "@fartlabs/declarative/common/jsonld";
 import {
   jsonSchemaDecoratorFactoryOfFile,
-  type ValueJSONSchema,
+  jsonSchemaOf,
 } from "@fartlabs/declarative/common/json-schema";
-import jsonld from "jsonld";
 import { Ajv } from "ajv";
 
 const jsonSchema = await jsonSchemaDecoratorFactoryOfFile(import.meta.url);
@@ -12,37 +9,22 @@ const jsonSchema = await jsonSchemaDecoratorFactoryOfFile(import.meta.url);
 @jsonSchema()
 export class Contract {
   public constructor(
-    public id: number,
+    public id: string,
     public teamName: string,
+    public teamLead: string,
     public projectIdea: string,
-    public members: number[],
+    public members: { name: string; role: string; email: string }[],
     public firstPrototypeDate: string,
     public finalProductDate: string,
     public decisionMethod: "Majority Vote" | "Representative Decision",
     public customRules: string,
     public signedAgreement: boolean,
   ) {}
+
+  public validate() {
+    const validator = ajv.compile(jsonSchemaOf(Contract));
+    return validator(this);
+  }
 }
 
-if (import.meta.main) {
-  const contract = new Contract(
-    423232113,
-    "Ash Ketchum",
-    "Pokemon fetch project",
-    [776735858],
-    new Date().toISOString(),
-    new Date().toISOString(),
-    "Majority Vote",
-    "Anybody not agreeing to the decision method will not be thrown out of the team.",
-    true,
-  );
-  const expandedContract = await jsonld.expand(docOf(contract));
-  console.log(expandedContract);
-
-  const ajv = new Ajv();
-  const validate = ajv.compile(
-    getPrototypeValue<ValueJSONSchema>(Contract)?.jsonSchema,
-  );
-  const isValid = validate(contract);
-  console.log(isValid);
-}
+const ajv = new Ajv();
--- models/teamMember.ts
+++ models/teamMember.ts
@@ -1,10 +1,7 @@
-import { getPrototypeValue } from "@fartlabs/declarative";
-import { docOf } from "@fartlabs/declarative/common/jsonld";
 import {
   jsonSchemaDecoratorFactoryOfFile,
-  type ValueJSONSchema,
+  jsonSchemaOf,
 } from "@fartlabs/declarative/common/json-schema";
-import jsonld from "jsonld";
 import { Ajv } from "ajv";
 
 const jsonSchema = await jsonSchemaDecoratorFactoryOfFile(import.meta.url);
@@ -12,27 +9,16 @@ const jsonSchema = await jsonSchemaDecoratorFactoryOfFile(import.meta.url);
 @jsonSchema()
 export class TeamMember {
   public constructor(
-    public id: number,
+    // public id: string,
     public name: string,
     public role: string,
     public email: string,
   ) {}
+
+  public validate() {
+    const validator = ajv.compile(jsonSchemaOf(TeamMember));
+    return validator(this);
+  }
 }
 
-if (import.meta.main) {
-  const member = new TeamMember(
-    776735858,
-    "Ash Ketchum",
-    "pokemon trainer",
-    "ashketchum@gmail.com",
-  );
-  const expandedMember = await jsonld.expand(docOf(member));
-  console.log(expandedMember);
-
-  const ajv = new Ajv();
-  const validate = ajv.compile(
-    getPrototypeValue<ValueJSONSchema>(TeamMember)?.jsonSchema,
-  );
-  const isValid = validate(member);
-  console.log(isValid);
-}
+const ajv = new Ajv();
